a4f546592823da3d06b4f5f53810bf77c818f228
Matěj Bucek <admin@mbucek.cz>
2022-01-14 22:29:15 +0100

    EME4J: Added some mathematical constants and functions.

--- src/main/java/cz/mbucek/eme4j/constants/Constants.java
+++ src/main/java/cz/mbucek/eme4j/constants/Constants.java
@@ -4,4 +4,10 @@ public class Constants {
 
 	@ConstantDescriptor(signature = {"PI", "π"})
 	public static final double PI = Math.PI;
+	
+	@ConstantDescriptor(signature = "φ")
+	public static final double GR = 1.6180339887498948482;
+	
+	@ConstantDescriptor(signature = "e")
+	public static final double E = Math.E;
 }
--- src/main/java/cz/mbucek/eme4j/functions/Functions.java
+++ src/main/java/cz/mbucek/eme4j/functions/Functions.java
@@ -7,4 +7,10 @@ public class Functions {
 	
 	@FunctionDescriptor(signature = "cos", args = 1)
 	public static final Function COS = (x) -> Math.cos(x[0]);
+	
+	@FunctionDescriptor(signature = "tan", args = 1)
+	public static final Function TAN = (x) -> Math.tan(x[0]);
+	
+	@FunctionDescriptor(signature = "cotg", args = 1)
+	public static final Function COTG = (x) -> 1 / Math.tan(x[0]);
 }
